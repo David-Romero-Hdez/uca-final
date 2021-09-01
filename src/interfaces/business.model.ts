6adc61d5932232ad3401ed5f6a01d3c397879ee6
David Romero <davidromero@davids-mbp.lan>
2021-08-31 20:30:07 -0500

    business model

--- src/interfaces/business.model.ts
+++ src/interfaces/business.model.ts
@@ -1,3 +1,18 @@
 export interface Business {
-
+    businessName: string,
+    phoneNumber: string,
+    email: string,
+    addressGroup: {
+        street: string,
+        number: string,
+        city: string,
+        municipality: string,
+    },
+    geopoint: {
+        latitude: number
+        longitude: number
+    },
+    description: string
+    type: string,
+    socialMedia: string[]
 }
